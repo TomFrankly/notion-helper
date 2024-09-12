9647ceb4b8e47a3d66b3109f8e34c4e7e311d5fa
Thomas Frank <thomasfrank09@gmail.com>
2024-09-12 11:00:49 -0500

    Updated index exports and imports

--- index.mjs
+++ index.mjs
@@ -4,7 +4,7 @@ import {
     block,
 } from "./blocks.mjs"
 import { setIcon } from "./emoji-and-files.mjs";
-import { page_meta, page_props, dbId, pageId, cover, icon, title, richText, checkbox, date, email, files, multiSelect, number, people, phoneNumber, relation, select, status, url } from "./page-meta.mjs";
+import { page_meta, page_props, parentDb, parentPage, pageId, blockId, propertyId, cover, icon, title, richText, checkbox, date, email, files, multiSelect, number, people, phoneNumber, relation, select, status, url } from "./page-meta.mjs";
 import { quickPages, createNotion } from "./pages.mjs";
 
 const NotionHelper = {
