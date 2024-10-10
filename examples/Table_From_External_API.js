82b3b3ada69411efbde623f3a9c6f1d3d8fbcd68
Thomas Frank <thomasfrank09@gmail.com>
2024-10-10 15:12:36 -0500

    Added examples directory

---
+++ examples/Table_From_External_API.js
@@ -0,0 +1,40 @@
+import { Client } from "@notionhq/client";
+import { createNotion, appendBlocks } from "notion-helper";
+import ky from "ky";
+
+const secret = "YOUR_INTEGRATION_KEY"
+
+const notion = new Client({
+    auth: secret,
+});
+
+const response = await appendBlocks({
+    block_id: page_id,
+    children: createNotion({ limitChildren: false })
+        .table({
+            has_column_header: true,
+            rows: [["Name", "HP", "ATK", "DEF"]],
+        })
+        .loop(
+            (pokemon, data) => {
+                pokemon.tableRow([
+                    data.name,
+                    data.stats[0].base_stat,
+                    data.stats[1].base_stat,
+                    data.stats[2].base_stat,
+                ]);
+            },
+            await Promise.all(
+                Array.from({ length: 10 }, (_, i) => {
+                    return ky
+                        .get(`https://pokeapi.co/api/v2/pokemon/${i + 1}`)
+                        .json();
+                })
+            )
+        )
+        .endTable()
+        .build(),
+    client: notion,
+});
+
+console.log(response)
