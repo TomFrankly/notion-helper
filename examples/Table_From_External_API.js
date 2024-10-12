e292ab03961253a3d616c808ee50ae520fedce5a
Thomas Frank <thomasfrank09@gmail.com>
2024-10-12 08:51:39 -0500

    Fixed example

--- examples/Table_From_External_API.js
+++ examples/Table_From_External_API.js
@@ -2,42 +2,43 @@ import { Client } from "@notionhq/client";
 import { createNotion, appendBlocks } from "notion-helper";
 import ky from "ky";
 
-const secret = "YOUR_INTEGRATION_KEY"
+const secret = "YOUR_INTEGRATION_KEY";
 
 const notion = new Client({
     auth: secret,
 });
 
-const page_id = "YOUR_TARGET_PAGE"
+const page_id = "YOUR_TARGET_PAGE";
+
+const pokemon = createNotion({ limitChildren: false })
+    .table({
+        has_column_header: true,
+        rows: [["Name", "HP", "ATK", "DEF"]],
+    })
+    .loop(
+        (pokemon, data) => {
+            pokemon.tableRow([
+                data.name,
+                data.stats[0].base_stat,
+                data.stats[1].base_stat,
+                data.stats[2].base_stat,
+            ]);
+        },
+        await Promise.all(
+            Array.from({ length: 10 }, (_, i) => {
+                return ky
+                    .get(`https://pokeapi.co/api/v2/pokemon/${i + 1}`)
+                    .json();
+            })
+        )
+    )
+    .endTable()
+    .build();
 
 const response = await appendBlocks({
     block_id: page_id,
-    children: createNotion({ limitChildren: false })
-        .table({
-            has_column_header: true,
-            rows: [["Name", "HP", "ATK", "DEF"]],
-        })
-        .loop(
-            (pokemon, data) => {
-                pokemon.tableRow([
-                    data.name,
-                    data.stats[0].base_stat,
-                    data.stats[1].base_stat,
-                    data.stats[2].base_stat,
-                ]);
-            },
-            await Promise.all(
-                Array.from({ length: 10 }, (_, i) => {
-                    return ky
-                        .get(`https://pokeapi.co/api/v2/pokemon/${i + 1}`)
-                        .json();
-                })
-            )
-        )
-        .endTable()
-        .build()
-        .content,
+    children: pokemon.content,
     client: notion,
 });
 
-console.log(response)
+console.log(response);
